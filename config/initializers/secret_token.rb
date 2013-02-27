97adffb3229165777ffb127158550257a95e2715
Abhi Ramesh <abhiramesh@gmail.com>
2013-02-26 17:51:34 -0800

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kargod::Application.config.secret_token = 'b0b3240d2c47dc1d3f469730291c415e1533153408386220743717cd6eca3f8669dedcb9d5d2bd8912007aed91a00235dc0b98624b798b4b5455f420bbe31f92'
